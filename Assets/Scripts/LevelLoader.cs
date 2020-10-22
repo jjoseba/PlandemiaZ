64baea53aa18eb83666100ad00f96e175c201a56
Joseba S <jjoseba@gmail.com>
2020-10-23 01:48:05 +0200

    Added main menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public float transitionTime = 1f;
+    public Animator transition;
+
+    public void FadeAndLoadScene(string sceneName)
+    {
+        StartCoroutine(LoadLevel(sceneName));
+    }
+
+    public void FadeAnExit()
+    {
+        StartCoroutine(ExitGame());
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        transition.SetTrigger("Start");
+        Debug.Log("Crossfading");
+        yield return new WaitForSeconds(transitionTime);
+
+        Debug.Log("Load level!" + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator ExitGame()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        Application.Quit();
+    }
+
+}
